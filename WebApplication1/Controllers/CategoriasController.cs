89e769d72f0244bc2d792402884e1c1ce2294fa3
raqueldossantosgarcia@gmail.com <raqueldossantosgarcia@gmail.com>
2022-05-31 22:12:06 -0300

    index do controlador categoria

---
+++ WebApplication1/Controllers/CategoriasController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private static IList<Categoria> cat = new List<Categoria>()
+        {
+            new Categoria() {CategoriaId = 1, Nome ="notebooks"},
+            new Categoria() {CategoriaId = 2, Nome = "monitores"},
+            new Categoria() {CategoriaId = 3, Nome=  "desktops"},
+        };
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(cat);
+        }
+    }
+}
